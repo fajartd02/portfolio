61eedb3900c4f5ad8628f151a400deb82596968f
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2023-04-04 23:20:57 +0700

    adding Chakra UI

---
+++ pages/ho/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Ho() {
+  return (
+    <div>
+
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+
+    </div>
+  )
+}
+
+export default Ho;
