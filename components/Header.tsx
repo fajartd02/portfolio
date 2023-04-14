470c56965f2d26e6368739f84dd912ff1aa1c7ec
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2023-04-14 23:36:48 +0700

    create a new header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,144 @@
-import { Box, Button, ButtonGroup, Flex, Heading, Spacer } from '@chakra-ui/react'
-import React from 'react'
+import { useState } from "react";
+import {
+  Flex,
+  Spacer,
+  Box,
+  Heading,
+  Button,
+  useColorModeValue,
+  useDisclosure,
+  IconButton,
+  useColorMode,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { HamburgerIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 function Header() {
-  return (
-    <Flex p={8}>
-        <Box>
-            <Heading size='md'>fajarhamka.</Heading>
-        </Box>
-        <Spacer />
-        <ButtonGroup gap='2'>
-            <Button colorScheme='teal'>About Me</Button>
-            <Button colorScheme='teal'>Skills</Button>
-            <Button colorScheme='teal'>Experiences</Button>
-            <Button colorScheme='teal'>Portfolio</Button>
-        </ButtonGroup>
-    </Flex>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen: isDrawerOpen, onOpen, onClose } = useDisclosure();
 
-export default Header
+  const handleToggle = () => setIsOpen(!isOpen);
+
+  const links = [
+    {
+      href: "/",
+      label: "Home",
+    },
+    {
+      href: "/about",
+      label: "About",
+    },
+    {
+      href: "/projects",
+      label: "Projects",
+    },
+    {
+      href: "/contact",
+      label: "Contact",
+    },
+  ];
+
+  return (
+    <Box
+      position="fixed"
+      top="0"
+      left="0"
+      right="0"
+      bg={useColorModeValue("white", "gray.800")}
+      borderBottom="1px"
+      borderColor={useColorModeValue("gray.200", "gray.900")}
+      width="full"
+      zIndex="10"
+    >
+      <Flex
+        align="center"
+        justify="space-between"
+        maxW="1200px"
+        margin="0 auto"
+        py="4"
+        px="6"
+      >
+        <Box>
+          <Heading as="h1" size="lg">
+            fajarhamka.
+          </Heading>
+        </Box>
+        <Box display={{ base: "none", md: "block" }}>
+          <Stack direction="row" spacing="4">
+            {links.map((link) => (
+              <Button
+                key={link.href}
+                variant="ghost"
+                _hover={{ textDecoration: "none" }}
+              >
+                {link.label}
+              </Button>
+            ))}
+          </Stack>
+        </Box>
+        <Box>
+          {isOpen ? null : (
+            <Box display={{ base: "none", md: "block" }}>
+              <IconButton
+                aria-label="Toggle dark mode"
+                icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+                onClick={toggleColorMode}
+                size="md"
+                ml={{ md: "4" }}
+              />
+            </Box>
+          )}
+          <Box display={{ base: "block", md: "none" }}>
+            <IconButton
+              aria-label="Toggle navigation"
+              icon={<HamburgerIcon />}
+              size="md"
+              onClick={onOpen}
+              display={isOpen ? "none" : "flex"}
+            />
+          </Box>
+        </Box>
+      </Flex>
+      <Drawer isOpen={isDrawerOpen} placement="right" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton display="none" />
+          <DrawerHeader>Menu</DrawerHeader>
+          <DrawerBody>
+            <Stack spacing="4">
+              {links.map((link) => (
+                <Button
+                  key={link.href}
+                  variant="ghost"
+                  onClick={onClose}
+                  width="full"
+                >
+                  {link.label}
+                </Button>
+              ))}
+              <Button
+                variant="ghost"
+                width="full"
+                onClick={() => {
+                  toggleColorMode();
+                  onClose();
+                }}
+              >
+                Toggle dark mode
+              </Button>
+            </Stack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </Box>
+  );
+}
+export default Header;
