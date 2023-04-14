e3ee6b2b0265860ed6f5582576cbc6e430f5ddb4
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2023-04-14 23:37:31 +0700

    modified index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
+import Hero from '@/components/Hero'
 import { Button, Heading, useColorMode } from '@chakra-ui/react'
 
 export default function Home() {
   // const { colorMode, toggleColorMode } = useColorMode()
   return (
     <>
-      
+      <Hero />
     </>
   )
 }
